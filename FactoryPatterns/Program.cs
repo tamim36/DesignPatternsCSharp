011103fdee809ba2de83cdbf3837c514bda272c4
tamim36 <tamimarefinanik@gmail.com>
2021-03-03 01:14:38 +0600

    initial commit

---
+++ FactoryPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
